0c72b0da16fa574a8b13ab0f4f9ed683c79e6dad
notJunius <120129256+notJunius@users.noreply.github.com>
2025-06-21 11:53:05 -0600

    started py functions file

---
+++ functions/run_python_file.py
@@ -0,0 +1,7 @@
+import os
+
+def run_python_file(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(abs_working_dir, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
